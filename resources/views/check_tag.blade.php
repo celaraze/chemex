f6aa20021ae369c6324c8d57ebdd397dfa5b80db
Xpesir <1964391+Xpesir@user.noreply.gitee.com>
2022-06-24 08:25:19 +0000

    新增一个根据传入的资产编号查询设备明细的页，更新bootstrap.css

--- resources/views/check_tag.blade.php
+++ resources/views/check_tag.blade.php
@@ -7,14 +7,78 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="all,follow">
-	<link rel="stylesheet" href="{{asset("static/css/checktag/bootstrap.css")}}" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset("static/css/checktag/main.css")}}" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{asset("static/CSS/bootstrap.css")}}" crossorigin="anonymous">
+    <style>
+        table {
+            color: #666;
+            font-size: .9em
+        }
+
+        .page {
+            background: #eef5f9
+        }
+
+        .has-shadow {
+            -webkit-box-shadow: 2px 2px 2px rgba(0,0,0,.1),-1px 0 2px rgba(0,0,0,.05);
+            box-shadow: 2px 2px 2px rgba(0,0,0,.1),-1px 0 2px rgba(0,0,0,.05)
+        }
+
+        .login-page {
+            position: relative
+        }
+
+        .tag-page .container {
+            position: relative;
+            z-index: 999;
+            padding: 20px;
+            min-height: 100vh
+        }
+
+        .tag-page .tag-holder {
+            overflow: hidden;
+            margin-bottom: 50px;
+            width: 100%;
+            border-radius: 5px
+        }
+
+        .tag-page .tag-holder .tag,.tag-page .tag-holder .info {
+            padding: 40px;
+            height: 100%;
+            min-height: 40vh
+        }
+
+        .tag-page .tag-holder div[class*=col-] {
+            padding: 0
+        }
+
+        .tag-page .tag-holder .info {
+            background: #c9261d;
+            color: #fff
+        }
+
+        .tag-page .tag-holder .info h1 {
+            font-weight: 600;
+            font-size: 2.5em
+        }
+
+        .tag-page .tag-holder .info p {
+            font-weight: 300
+        }
+
+        body {
+            background-color: #fff;
+            color: #212529;
+            font-weight: 400;
+            font-size: 1rem;
+            font-family: Poppins,sans-serif
+        }
+    </style>
     <link id="new-stylesheet" rel="stylesheet">
 </head>
 <body>
-    <div class="page login-page">
+    <div class="page tag-page">
         <div class="container d-flex align-items-center">
-            <div class="form-holder has-shadow">
+            <div class="tag-holder has-shadow">
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="info d-flex align-items-center">
@@ -27,7 +91,7 @@
                         </div>
                     </div>
                     <div class="col-lg-6 bg-white">
-                        <div class="form d-flex align-items-center">
+                        <div class="tag d-flex align-items-center">
                             <div class="content">
 								<table>
                     <tbody>
