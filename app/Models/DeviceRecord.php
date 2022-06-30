c70edfa8307f51236aed4c27fec83d63d722318d
Xpesir <1964391+Xpesir@user.noreply.gitee.com>
2022-06-30 10:27:49 +0000

    设备列表报废的显示

--- app/Models/DeviceRecord.php
+++ app/Models/DeviceRecord.php
@@ -40,6 +40,7 @@ use Illuminate\Translation\Translator;
  * @property int parent_id
  * @property int order
  * @property string field
+ * @property string discard_at
  */
 class DeviceRecord extends Model
 {
@@ -228,6 +229,10 @@ class DeviceRecord extends Model
      */
     public function status(): array|string|Translator|Application|null
     {
+        if(!empty($this -> discard_at)){
+            return ["<span class='badge badge-danger'>" . trans('main.discard') . "</span>", trans('main.discard')];
+        }
+
         if ($this->isLend()) {
             return ["<span class='badge badge-primary'>" . trans('main.lend') . "</span>", trans('main.lend')];
         }
