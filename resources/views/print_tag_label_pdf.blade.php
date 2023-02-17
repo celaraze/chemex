1219d584ee240842e97efb3b78be697f0ed39cc0
Xpesir <1964391+Xpesir@user.noreply.gitee.com>
2023-02-17 06:52:40 +0000

    修正标签打印二维码URL错误
    Signed-off-by: Xpesir <1964391+Xpesir@user.noreply.gitee.com>

--- resources/views/print_tag_label_pdf.blade.php
+++ resources/views/print_tag_label_pdf.blade.php
@@ -33,10 +33,10 @@ div.qr{
         <div class="asset_nember" >{{$data['asset_number']}}</div>
         <div class="title" >Chemex IT资产管理系统</div>
         <div class="data" >名称：{{$data['name']}}</div>
-        <div class="data" >类型：{{$data['asset_number']}}</div>
+        <div class="data" >类型：{{$data['category']}}</div>
         <div class="data" >型号：{{$data['asset_number']}}</div>
     </div>
     <div class="qr">
-        <qrcode value="{{url()->previous();}}/api/asset_card/device/{{$data['asset_number']}}" ec="L" style="border: none; width:{{$width*0.25}}mm; height: {{$width*0.25}}mm;"></qrcode>
+        <qrcode value="{{$_SERVER["HTTP_HOST"];}}/api/asset_card/device/{{$data['asset_number']}}" ec="L" style="border: none; width:{{$width*0.25}}mm; height: {{$width*0.25}}mm;"></qrcode>
     </div>
 </page>
